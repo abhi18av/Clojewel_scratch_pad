612723f83d33e4c7bbc2ee8dc4ea8b1d10ec86c2
Abhinav Sharma <abhi18av@gmail.com>
2017-07-30 23:21:56 +0530

    Created a method to print the sexpression of the content read from file

--- _experiments/fileConversion.jl
+++ _experiments/fileConversion.jl
@@ -3,13 +3,23 @@ f = open("LearnJuliaTest.jl","r")
 content = readstring(f)
 close(f)
 
+
+
 f = open("test1.jl","r")
 content = readstring(f)
 close(f)
 
 
-quoted_content = "quote " * content * " end"
 
-eval(eval(parse(quoted_content)))
+f = open("array.jl","r")
+content = readstring(f)
+close(f)
+
+quoted_content = "quote " * content * " end"
+expr = parse(quoted_content)
+
+Meta.show_sexpr(expr)
+
+#eval(eval(parse(quoted_content)))
 
 
