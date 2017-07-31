d83c2ded4c02726cfeb26e49c86e95938035bb47
Abhinav Sharma <abhi18av@gmail.com>
2017-07-31 10:29:53 +0530

    got a function isolated in a basic file

--- _experiments/fileConversion.jl
+++ _experiments/fileConversion.jl
@@ -1,27 +1,41 @@
 
-#println(ARGS[1])
-fileName = ARGS[1]
 
 
-function convertFile(fileName::String)
-f = open(fileName,"r")
+function convertFile(input_file_name::String)
+f = open(input_file_name,"r")
 content = readstring(f)
 close(f)
 
 quoted_content = "quote " * content * " end"
 expr = parse(quoted_content)
 
-Meta.show_sexpr(expr)
 
+#Meta.show_sexpr(expr)
+
+#string(Meta.show_sexpr(expr))
+
+#return expr
+return Meta.show_sexpr(expr)
 
 end
 
 
 #eval(eval(parse(quoted_content)))
 
+#println(ARGS[1])
 
-convertFile(fileName)
+input_file_name = ARGS[1]
+convertFile(input_file_name)
 
+#convertFile("io.jl")
+
+#output_expr = convertFile(input_file_name)
+
+#output_str = string(output_expr)
+
+
+#output_file_name = split(input_file_name, ".")[1] * ".jls"
+#write(output_file_name, output_expr)
 
 
 
