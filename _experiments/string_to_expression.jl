acc433458246e7cb169127f22b4c3457286d5610
Abhinav Sharma <abhi18av@gmail.com>
2017-07-31 13:11:59 +0530

    making progress with the macro magic in Julia - I'm truly in love!

--- _experiments/string_to_expression.jl
+++ _experiments/string_to_expression.jl
@@ -17,9 +17,18 @@ return (x + 9)
 end"
 
 
+add9_2_parsed = parse(add9_2)
+
+
+add9_2_parsed_evald = eval(add9_2_parsed)
+
 parse(add9_2)
 
+julia> add9_1 == add9_2_parsed_evald
+false
 
+julia> add9_1(9) == add9_2_parsed_evald(9)
+true
 
 
 eval(Expr(:call, parse(add9_2), :9))
@@ -27,7 +36,23 @@ eval(Expr(:call, parse(add9_2), :9))
 eval(Expr(:call, parse(add9_2), 9))
 
 
+julia> code_lowered(add9_1)[1]
+LambdaInfo template for (::##1#2)(x::Int64) at REPL[1]:3
+:(begin
+        nothing
+        return x + 9
+    end)
 
+julia> code_lowered(add9_2_parsed_evald)[1]
+LambdaInfo template for (::##5#6)(x::Int64) at none:4
+:(begin
+        nothing
+        return x + 9
+    end)
+
+
+
+code_lowered(add9_1)[1] == code_lowered(add9_2_parsed_evald)
 
 julia> code_lowered(add9_1)
 1-element Array{LambdaInfo,1}:
@@ -44,6 +69,13 @@ function eval_expr(ex )
 eval(Expr(ex))
 
 end
+
+
+julia> code_typed(add9_1)[1]
+LambdaInfo for (::##1#2)(::Int64)
+:(begin
+        return (Base.box)(Int64,(Base.add_int)(x,9))
+    end::Int64)
 
 
 julia> code_lowered(add9)[1]
