b30baadd33e097781a5c0889f8c4df18f5937732
Abhinav Sharma <abhi18av@gmail.com>
2017-08-02 00:02:58 +0530

    Yup, the expression evaluation works fine now.

--- project-1/src/project_1/core.clj
+++ project-1/src/project_1/core.clj
@@ -1,16 +1,34 @@
 (ns project-1.core
+;  (:require '[[clojure.java.shell :only [sh]]])
   (:gen-class))
 
-(use '[clojure.java.shell :only [sh]])
+;(use '[clojure.java.shell :only [sh]])
+
+; This command is there to change the repl :ns
+;  (ns project-1.core)
+
+
+;(def julia-exec " /Applications/JuliaPro-0.5.2.1.app/Contents/Resources/julia/Contents/Resources/julia/bin/julia ")
+;(sh julia-exec " -e  \" println(1 + 1) \" ")
+;(def julia-eval " /Applications/JuliaPro-0.5.2.1.app/Contents/Resources/julia/Contents/Resources/julia/bin/julia -e \"println(1 + 1)\"")
 
 
 
-(def julia-exec "/Applications/JuliaPro-0.5.2.1.app/Contents/Resources/julia/Contents/Resources/julia/bin/julia ")
+(use '[me.raynes.conch :refer [programs with-programs let-programs] :as sh])
+
+(sh/programs julia )
+;(julia "--help" {:seq true})
+(julia "-e" "println(1+1)")
+
+;(julia "-e" "eval(Expr(:call, :println, (:call, :+, 1, 1)))")
 
 
-;(sh "julia --help")
+(def expr "eval(Expr(:call, :print, Expr(:call, :+, 1, 1)))")
 
-;(sh julia-exec "-e  \" println(1 + 1) \" ")
+(julia "-e" expr)
+
+
+
 
 
 (defn -main
