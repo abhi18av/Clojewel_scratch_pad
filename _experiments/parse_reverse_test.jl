ca4732db5043dda8fad3d290989e0d3e8f12a604
Abhinav Sharma <abhi18av@gmail.com>
2017-08-03 17:25:25 +0530

    Will convert this to a proper file oriented function now

--- _experiments/parse_reverse_test.jl
+++ _experiments/parse_reverse_test.jl
@@ -1,5 +1,3 @@
-
-
 # parse_reverse: parse expr in reverse :p 
 function parse_reverse(S_expr::Tuple)
     return Expr( Any[isa(i, Tuple) ? parse_reverse(i) : i for i in S_expr]... );
@@ -19,15 +17,19 @@ seek(buffer, 0);                      # 'rewind' buffer
 SExprStr = readstring(buffer);      # get buffer contents as string
 close(buffer);                # please to be closink after finished, da?
 
-#show(SExprStr)
 return SExprStr
 end
 
-
+#println("\n\n")
 
 expr = :(function add9( x) x + 9 end);            # the expr we want to generate an s_expr for
+#show(expr)
+
+
 SExprStr = parse_expr(expr)
+println("\n\n")
 
 SExpr = parse(SExprStr) |> eval;
-#show(SExpr)
+show(SExpr)
+println("\n\n")
 parse_reverse(SExpr) |> show
