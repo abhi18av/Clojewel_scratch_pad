9d5016961c7781c65ad6d60cbdeed959e755a614
Abhinav Sharma <abhi18av@gmail.com>
2017-08-03 17:19:16 +0530

    Making progress!

--- _experiments/parse_reverse_test.jl
+++ _experiments/parse_reverse_test.jl
@@ -4,12 +4,14 @@
 function parse_reverse(S_expr::Tuple)
     return Expr( Any[isa(i, Tuple) ? parse_reverse(i) : i for i in S_expr]... );
 end
-    
-    
-buffer = IOBuffer();              # will use to 'capture' the s_expr in
-expr = :(function add9( x) x + 9 end);            # the expr we want to generate an s_expr for
+ 
 
-Meta.show_sexpr(expr)
+
+function parse_expr(expr::Expr)    
+buffer = IOBuffer();              # will use to 'capture' the s_expr in
+#expr = :(function add9( x) x + 9 end);            # the expr we want to generate an s_expr for
+
+#Meta.show_sexpr(expr)
 Meta.show_sexpr(buffer, expr);       # push s_expr into buffer buffer
     
     
@@ -17,7 +19,15 @@ seek(buffer, 0);                      # 'rewind' buffer
 SExprStr = readstring(buffer);      # get buffer contents as string
 close(buffer);                # please to be closink after finished, da?
 
-show(SExprStr)
+#show(SExprStr)
+return SExprStr
+end
+
+
+
+expr = :(function add9( x) x + 9 end);            # the expr we want to generate an s_expr for
+SExprStr = parse_expr(expr)
+
 SExpr = parse(SExprStr) |> eval;
-show(SExpr)
+#show(SExpr)
 parse_reverse(SExpr) |> show
