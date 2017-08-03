1a4492baed8036ed131cded1658ba19404b371bc
Abhinav Sharma <abhi18av@gmail.com>
2017-08-03 17:10:57 +0530

    working on parse_reverse and moved to clojure1.9

--- _experiments/parse_reverse.jl
+++ _experiments/parse_reverse.jl
@@ -6,14 +6,18 @@ function parse_reverse(S_expr::Tuple)
 end
     
     
-B  = IOBuffer();              # will use to 'capture' the s_expr in
-Expr1 = :(function add9( x) x + 9 end);            # the expr we want to generate an s_expr for
-Meta.show_sexpr(B, Expr1);       # push s_expr into buffer B
-    
-    
-seek(B, 0);                      # 'rewind' buffer
-SExprStr = readstring(B);      # get buffer contents as string
-close(B);                        # please to be closink after finished, da?
-SExpr = parse(SExprStr) |> eval;
+buffer = IObufferuffer();              # will use to 'capture' the s_expr in
+expr = :(function add9( x) x + 9 end);            # the expr we want to generate an s_expr for
 
+Meta.show_sexpr(expr)
+Meta.show_sexpr(buffer, expr);       # push s_expr into buffer buffer
+    
+    
+seek(buffer, 0);                      # 'rewind' buffer
+SExprStr = readstring(buffer);      # get buffer contents as string
+close(buffer);                # please to be closink after finished, da?
+
+show(SExprStr)
+SExpr = parse(SExprStr) |> eval;
+show(SExpr)
 parse_reverse(SExpr) |> show
