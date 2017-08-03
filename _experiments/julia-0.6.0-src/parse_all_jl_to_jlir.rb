99079154855d16164a5bae66c81d0cd9b8beba71
Abhinav Sharma <abhi18av@gmail.com>
2017-08-03 14:36:44 +0530

    Updated the ruby driver script for the new julia binary

--- _experiments/julia-0.6.0-src/parse_all_jl_to_jlir.rb
+++ _experiments/julia-0.6.0-src/parse_all_jl_to_jlir.rb
@@ -5,7 +5,7 @@ for f in files
 	oldFile = f.split(".")[0]
 	newFile = oldFile + ".jlir"
 
-	jl_exec = "/Applications/JuliaPro-0.5.2.1.app/Contents/Resources/julia/Contents/Resources/julia/bin/julia "
+	jl_exec = "/usr/local/bin/julia"
 	cmd = jl_exec + " jl_file_to_s_expr.jl " + f + " > " + newFile
 	system(cmd)
 
