c3f19ab1e48bcf0c1f7ccf0a5d57cca5f260e323
Abhinav Sharma <abhi18av@gmail.com>
2017-08-03 16:58:06 +0530

    Commiting with the correct code

--- _experiments/stackOverflow.jl
+++ _experiments/stackOverflow.jl
@@ -1,39 +1,7 @@
 
 
 
-
-# corrected code
-
-
 """rxpe_esrap: parse expr in reverse :p """
-function parse_reverse(S_expr::Tuple)
-    return Expr( Any[isa(i, Tuple) ? rpxe_esrap(i) : i for i in S_expr]... );
-end
-    
-
-parse_expr(Expr1)  
-
-B     = IOBuffer();              # will use to 'capture' the s_expr in
-Expr1 = :(1 + 2 * 3);            # the expr we want to generate an s_expr for
-#Meta.show_sexpr(B, Expr1);       # push s_expr into buffer B
-    
-    
-seek(B, 0);                      # 'rewind' buffer
-SExprStr = readstring(B);      # get buffer contents as string
-close(B);       
-return(SExprStr)
-end
-                 # please to be closink after finished, da?
-SExpr = parse_expr(SExprStr) |> eval;
-
-parse_reverse(SExpr) |> show
-
-
-
-################
-
-"""
-#rxpe_esrap: parse expr in reverse :p 
 function rpxe_esrap(S_expr::Tuple)
   return Expr( Tuple( isa(i, Tuple) ? rpxe_esrap(i) : i for i in S_expr )... );
 end
@@ -55,7 +23,27 @@ SExpr = parse(SExprStr) |> eval; # final s_expr in tuple form
 
 SExpr
 rpxe_esrap(SExpr)
-"""
+
 
 ##############
 
+# corrected code
+
+
+"""rxpe_esrap: parse expr in reverse :p """
+function rpxe_esrap(S_expr::Tuple)
+    return Expr( Any[isa(i, Tuple) ? rpxe_esrap(i) : i for i in S_expr]... );
+end
+    
+    
+B     = IOBuffer();              # will use to 'capture' the s_expr in
+Expr1 = :(1 + 2 * 3);            # the expr we want to generate an s_expr for
+Meta.show_sexpr(B, Expr1);       # push s_expr into buffer B
+    
+    
+seek(B, 0);                      # 'rewind' buffer
+    SExprStr = readstring(B);      # get buffer contents as string
+close(B);                        # please to be closink after finished, da?
+SExpr = parse(SExprStr) |> eval;
+
+rpxe_esrap(SExpr) |> show
