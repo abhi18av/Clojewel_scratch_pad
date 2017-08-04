59cb552e157cfa9642565384717c931882c7355e
Abhinav Sharma <abhi18av@gmail.com>
2017-08-04 10:04:53 +0530

    working on the julia repl network

--- _experiments/stackOverflow.jl
+++ _experiments/stackOverflow.jl
@@ -34,13 +34,13 @@ rpxe_esrap(SExpr)
 function rpxe_esrap(S_expr::Tuple)
     return Expr( Any[isa(i, Tuple) ? rpxe_esrap(i) : i for i in S_expr]... );
 end
-    
-    
-B     = IOBuffer();              # will use to 'capture' the s_expr in
+
+
+B = IOBuffer();              # will use to 'capture' the s_expr in
 Expr1 = :(function add9( x) x + 9 end);            # the expr we want to generate an s_expr for
 Meta.show_sexpr(B, Expr1);       # push s_expr into buffer B
-    
-    
+
+
 seek(B, 0);                      # 'rewind' buffer
     SExprStr = readstring(B);      # get buffer contents as string
 close(B);                        # please to be closink after finished, da?
