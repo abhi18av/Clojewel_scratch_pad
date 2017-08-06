53c160cb9a965e7aca640666995bff281efc6d22
Abhinav Sharma <abhi18av@gmail.com>
2017-08-06 12:11:40 +0530

    working on writing a julia scratchpad

--- clojewel/src/clojewel/core.clj
+++ clojewel/src/clojewel/core.clj
@@ -6,28 +6,25 @@
 ;(use '[me.raynes.conch :refer [programs with-programs let-programs] :as sh])
 
 
-
-
 ;; Import and Check the julia version
 
 (do
-  (sh/programs julia )
+  (sh/programs julia)
   (julia "--version" {:seq true}))
-
-
-
-
 
 ; Clearly this can be converted into a Julia expression symbolically.
 
-(julia "-e" "println(1+1)")
+;;  (julia "-e" "println(1+1)")
+;;  (def expr "eval(Expr(:call, :print, Expr(:call, :+, 1, 1)))")
+;;  (julia "-e" expr)
 
-(def expr "eval(Expr(:call, :print, Expr(:call, :+, 1, 1)))")
+(defn sh-julia-eval-string [julia-expression]
+  (julia "-e" julia-expression))
+(sh-julia-eval-string "println(1+1)")
+(sh-julia-eval-string "eval(Expr(:call, :print, Expr(:call, :+, 1, 1)))")
 
-(julia "-e" expr)
 
-
-
+;; Create a function that dumps the julia-string-expression into a julia_file and runs it with julia
 
 
 
