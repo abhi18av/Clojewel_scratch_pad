5d78803fb210d449e1dfdfca40adeb45bc009393
Abhinav Sharma <abhi18av@gmail.com>
2017-08-08 18:05:14 +0530

    Moved all the files from CLJS to src

--- clojewel/resources/filesFromCLJS/cljs_build/npm_deps_test/string_requires.cljs
+++
@@ -1,9 +0,0 @@
-(ns npm-deps-test.string-requires
-  (:require [react :refer [createElement]]
-            ["react-dom/server" :as ReactDOMServer]
-            [npm-deps-test.string-requires-in-classpath]))
-
-(enable-console-print!)
-
-(println "ReactDOMServer exists:" ReactDOMServer
-  (.-renderToString ReactDOMServer))
