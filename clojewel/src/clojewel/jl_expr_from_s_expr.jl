a2f52bddef773f7da3bbfb78787c26aa88a931d8
Abhinav Sharma <abhi18av@gmail.com>
2017-08-08 14:42:44 +0530

    I've become much more structured with clojure already

--- clojewel/src/clojewel/jl_expr_from_s_expr.jl
+++ clojewel/src/clojewel/jl_expr_from_s_expr.jl
@@ -2,7 +2,11 @@
 
 # parse_reverse: parse expr in reverse :p 
 function parse_reverse(S_expr::Tuple)
-    return Expr( Any[isa(i, Tuple) ? parse_reverse(i) : i for i in S_expr]... );
+
+    expr =  Any[isa(i, Tuple) ? parse_reverse(i) : i for i in S_expr]
+    #println(expr)
+    return Expr(expr... );
+
 end
  
 
--- clojewel/src/clojewel/jlrepl.clj
+++ clojewel/src/clojewel/jlrepl.clj
@@ -3,39 +3,49 @@
 
 (require '[clojewel.utils :as utils])
 
-
 ;; TODO Prefer the let form and move the variables closer to where they are used.
+
+
+;;;; SCRATCH RELATED FUNCTIONS
+
+(defn shell-save-to-scratch
+  "This function saves the julia expression to the scratch file"
+  [julia-expression]
+  (let [scratch-jl "./src/clojewel/scratch.jl"]
+    (utils/shell-save-to-file scratch-jl julia-expression)))
+
+(defn julia-create-scratch-jlir
+  "Convert the scratch file to it's JLIR version and show"
+  []
+  (let [driver-file  "./src/clojewel/file_to_jlir.jl"
+        source-file  "./src/clojewel/scratch.jl"
+        output-file  "./src/clojewel/scratch.jlir"]
+    (do
+      (utils/julia-eval-file-with-driver driver-file source-file output-file))))
+
+(defn shell-show-scratch
+  "Show the \"normal julia syntax\" contents of scratch.jl file"
+  []
+  (let [file "./src/clojewel/scratch.jl"]
+    (utils/shell-show-file-content file)))
+
+(defn shell-show-scratch-jlir
+  "Show the \"JLIR\" contents of scratch.jlir file"
+  []
+  (let [file "./src/clojewel/scratch.jlir"]
+    (utils/shell-show-file-content file)))
 
 (defn julia-eval-scratch
   "This function evaluates the scratch file using << julia scratch.jl >>"
   []
-  (utils/julia-eval-file "./src/clojewel/scratch.jl"))
+  (let [scratch-jl "./src/clojewel/scratch.jl"]
+    (utils/julia-eval-file scratch-jl)))
 
 
-; EXAMPLE
-;(clojewel.utils/shell-show-file-content "./src/clojewel/scratch.jl")
-(defn julia-save-string-to-scratch
-  "This function saves the julia expressoin to the scratch file"
+;;;; IN MEMORY RELATED EXPRESSIONS
+
+(defn shell-create-jl-from-jlir
+"This function takes in the julia JLIR form and uses a driver script to print out the JL "
   [julia-expression]
-  (utils/shell-save-to-file "./src/clojewel/scratch.jl" julia-expression))
 
-(defn julia-show-s-expr
-  "This function saves the julia expression to a file and then evals the file using the file convertor script and yields the s-expr"
-  [julia-expression]
-  (do
-    (julia-save-string-to-scratch julia-expression)
-    (utils/julia-eval-file-with-driver "./src/clojewel/file_to_jlir.jl" "./src/clojewel/scratch.jl" "./src/clojewel/scratch.jlir")
-    (utils/shell-show-file-content "./src/clojewel/scratch.jlir")))
-
-(defn julia-from-s-expr
-  "This function saves the s-expression to a file and then evals the file using the script and yields the julia-expression."
-  [expr])
-
-
-(defn jl-scratch-jlir
-; (julia {:in (str "./src/clojewel/file_to_jlir.jl" " " scratch-jl ) :seq true :verbose true}))
- [])
-
-(defn shell-open-scratch-files-in-sublime
-  [])
-
+  )
